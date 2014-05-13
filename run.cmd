307bc6689d7dad34bce0d6cd43267e9a15f600a6
Tyler Band <tylerm007@gmail.com>
2014-05-12 22:42:02 -0400

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,5 +1,3 @@
-//set mycp = ./;C:/projects/aws-java-sdk-1.7.8.1/thirdParty;c:/projects/aws-java-sdk-1.7.8.1/lib/aws-java-sdk-1.7.8.1.jar;C:/projects/aws-java-sdk-1.7.8.1/lib/rhino-1.7R4.jar
-
 
 java -cp target/lib/joda-time-2.2.jar;target/lib/jackson-annotations-2.1.1.jar;target/lib/jackson-databind-2.1.1.jar;target/lib/jackson-core-2.1.1.jar;;target/classes;target/lib/commons-logging-1.1.1.jar;target/lib/httpclient-4.2.3.jar;target/lib/httpcore-4.2.jar;target/lib/aws-java-sdk-1.7.8.1.jar;target/lib/rhino-1.7R4.jar;target/lib/commons-codec-1.3.jar;target/lib/mail-1.4.3.jar org.mozilla.javascript.tools.shell.Main test.js
 
